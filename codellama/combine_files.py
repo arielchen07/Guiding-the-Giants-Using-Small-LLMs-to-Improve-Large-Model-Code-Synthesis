23c80ff39edb48d2cd2459cd47e8aebc55c4564d
Kehan Li <kehanli@berkeley.edu>
2025-03-16 15:24:26 -0400

    removing unused codellama code files

--- codellama/combine_files.py
+++
@@ -1,9 +0,0 @@
-import json
-a = json.load(open("../data/human_eval_data_ambiguity_with_soln_new.json"))
-b = json.load(open("../data/code_llama_output_good.json"))
-for i in range(len(a)):
-    a[i]["solution"]= b[i]["generated_text"]
-responses_json = json.dumps(a, indent=4)  # indent=4 for pretty formatting
-
-with open(f"combined_code_llama_good.json", "w") as file:
-    file.write(responses_json)
